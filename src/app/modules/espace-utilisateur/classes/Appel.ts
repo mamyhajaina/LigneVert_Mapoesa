23c2871245ecc948edd4cb3b7dedbd212b34f2b9
Andriantsitohaina Hajaina <74816899+mamyhajaina@users.noreply.github.com>
2023-06-30 21:35:36 +0300

    [Liste Appel]

---
+++ src/app/modules/espace-utilisateur/classes/Appel.ts
@@ -0,0 +1,19 @@
+export class Appel {
+    idAppel: any;
+    nomAppelant: string | undefined;
+    numeroAppelant: string | undefined;
+    sexe: string | undefined;
+    resumeAppel: string | undefined;
+    idCategireAppel: any;
+    idProjet: any;
+    idVolet: any;
+    idActivites: any;
+    idDetailsActivites: any;
+    idRegion: any;
+    idDistrique: any;
+    categorieSocioProAppelant: string | undefined;
+    dateAppel: any;
+    dateModification: any;
+    idUtiliateur: any;
+    idPriorite: any;
+}
